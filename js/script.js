5a0b939c8a9d56c0e36a3da6f5bd324d39e7b19f
taddeoneri <taddeo.neri10@gmail.com>
2023-03-21 15:48:22 +0100

    result and score

--- js/script.js
+++ js/script.js
@@ -6,7 +6,10 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 
 
 let numbers = [];
+const flashNumbers = document.getElementById('numbers');
 const memory = document.getElementById('memory');
+const result = document.getElementById('result');
+const button = document.querySelector('button');
 
 while(numbers.length < 5){
     const number = getRandomInt(1, 100);
@@ -20,9 +23,38 @@ while(numbers.length < 5){
 }
 
 const myTimeout = setTimeout(function(){
-    const flashNumbers = document.getElementById('numbers');
     flashNumbers.classList.add('d-none');
-}, 30000);
+    result.classList.remove('d-none')
+}, 3000);
+
+button.addEventListener('click', resultNumber);
+
+function resultNumber(){
+    const resultNumber1 = parseInt(document.getElementById('number1').value);
+    const resultNumber2 = parseInt(document.getElementById('number2').value);
+    const resultNumber3 = parseInt(document.getElementById('number3').value);
+    const resultNumber4 = parseInt(document.getElementById('number4').value);
+    const resultNumber5 = parseInt(document.getElementById('number5').value);
+    let array = [];
+    let score = 0;
+    const par = document.querySelector('.score');
+    array.push(resultNumber1);
+    array.push(resultNumber2);
+    array.push(resultNumber3);
+    array.push(resultNumber4);
+    array.push(resultNumber5);
+    for(let i = 0; i < numbers.length; i++){
+        for(let j = 0; j < array.length; j++){
+            if(array[j] === (numbers[i])){
+                score++;
+            }
+        }
+    }
+    par.innerHTML += 
+    `
+        <h4>Il tuo risultato è: ${score}</h4>
+    `;
+}
 
 
 function getRandomInt(min, max) {
